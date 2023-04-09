c76478b57f7174974a81929d3e9a6b0cd2d60f42
Tomás <tomascan99@gmail.com>
2023-04-09 13:32:58 +0200

    Comunicacion indirecta final
    Workers
    Sensores
    Proxy
    Terminal

--- meteo_proxy.py
+++ meteo_proxy.py
@@ -1,6 +1,7 @@
 import redis
 import pika
 import time
+import pickle
 from datetime import datetime, timedelta
 
 # Conexión Redis
@@ -13,22 +14,28 @@ channel = connection.channel()
 # Crear exchange, recibe mensajes y los distribuye en colas (crear para distintas terminales)
 channel.exchange_declare(exchange='average', exchange_type='fanout')
 
-Y = 10
+Y = 3
 
 while True:
     # WELLNESS
     w_list = []
     w_string = r_client.lpop("wellness")
+
+    # Control empty Strings
     if w_string is None:
         print("No hay datos wellness")
         while True:
             time.sleep(Y)
             w_string = r_client.lpop("wellness")
             if w_string is not None:
+                time.sleep(Y)
                 break
 
     w_first = datetime.strptime(w_string.strip('()').split(" : ")[0], "%Y-%m-%d %H:%M:%S")
     w_limit = w_first + timedelta(seconds=Y)
+
+    print("WELLNESS.  Desde: ", w_first, "Hasta: ", w_limit)
+
     w_list.append(float(w_string.strip('()').split(" : ")[1]))
     while True:
         w_string = r_client.lpop("wellness")
@@ -40,8 +47,10 @@ while True:
             break
 
         w_list.append(float(w_string.strip('()').split(" : ")[1]))
+        print("wellness: ", w_time, w_list)
 
     w_avg = round(sum(w_list) / len(w_list), 3)
+    print("Media wellness: ", w_avg)
 
     # POLLUTION
     p_list = []
@@ -52,10 +61,14 @@ while True:
             time.sleep(Y)
             p_string = r_client.lpop("pollution")
             if p_string is not None:
+                time.sleep(Y)
                 break
 
     p_first = datetime.strptime(p_string.strip('()').split(" : ")[0], "%Y-%m-%d %H:%M:%S")
     p_limit = p_first + timedelta(seconds=Y)
+
+    print("POLLUTION.  Desde: ", p_first, "Hasta: ", p_limit)
+
     p_list.append(float(p_string.strip('()').split(" : ")[1]))
 
     while True:
@@ -68,12 +81,26 @@ while True:
             break
 
         p_list.append(float(p_string.strip('()').split(" : ")[1]))
+        print("pollution: ", p_time, p_list)
 
     p_avg = round(sum(p_list) / len(p_list), 3)
+    print("Media Pol:", p_avg)
+
+    # Serializa los datos
+    message = {
+        "wellness": {
+            "value": w_avg,
+            "time": w_time
+        },
+        "pollution": {
+            "value": p_avg,
+            "time": p_time
+        }
+    }
+    message_serialized = pickle.dumps(message)
 
     # Publica mensaje en el exchange (Crear para distintas terminales)
-    channel.basic_publish(exchange='average', routing_key='',
-                          body=f"Wellness: {w_avg}, {w_time}, Pollution: {p_avg}, {p_time}")
+    channel.basic_publish(exchange='average', routing_key='', body=message_serialized)
     time.sleep(Y)
 
 connection.close()
--- terminal.py
+++ terminal.py
@@ -1,14 +1,22 @@
 import pika
 import sys, os
+import pickle
+import matplotlib.pyplot as plt
+from matplotlib.dates import DateFormatter
+
+# Inicializar datos
+wellness_data = []
+pollution_data = []
+time_data = []
+
+# Crear gráfico
+fig, axs = plt.subplots()
 
 
 def main():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='localhost'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
     channel = connection.channel()
-
     channel.exchange_declare(exchange='average', exchange_type='fanout')
-
     # Crear cola temporal y conectarla al exchange
     result = channel.queue_declare(queue='', exclusive=True)
     queue_name = result.method.queue
@@ -16,12 +24,30 @@ def main():
 
     # Definir Callback
     def callback(ch, method, properties, body):
-        print(f"Recibido: {body}")
+        data = pickle.loads(body)
+        print(f"Recibido: {data}")
+
+        # Obtener los valores de la respuesta
+        wellness = data.get('wellness', {}).get('value')
+        pollution = data.get('pollution', {}).get('value')
+        time = data.get('wellness', {}).get('time')
+
+        # Agregar los valores a las listas
+        wellness_data.append(wellness)
+        pollution_data.append(pollution)
+        time_data.append(time)
+
+        # Actualizar el gráfico
+        axs.clear()
+        axs.plot_date(time_data, wellness_data, '-b', label='Wellness')
+        axs.plot_date(time_data, pollution_data, 'r', label='Pollution')
+        axs.set_title('Wellness and Pollution')
+        axs.legend()
+        axs.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d %H:%M:%S'))
+        plt.pause(0.01)
 
     # Consumir mensajes de la cola temporal
-
     channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
-
     print("Waiting for messages...")
 
     # Iniciar bucle de eventos de RabbitMQ
@@ -37,4 +63,3 @@ if __name__ == '__main__':
             sys.exit(0)
         except SystemExit:
             os._exit(0)
-
